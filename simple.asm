8d29fe056d9ff871d24d2ed99e4aad8e5a05de33
David Tomusk <dtomusk@gmail.com>
2020-06-28 11:09:58 +0100

    Added comments for each section and call

--- simple.asm
+++ simple.asm
@@ -2,29 +2,29 @@
 
 	section .text
 _start: 
-	mov 	rax, 1
+	mov 	rax, 1		; system call to write msg 
 	mov 	rdi, 1
 	mov	rsi, msg
 	mov	rdx, 16
 	syscall 
 	
-	mov	rax, 0
+	mov	rax, 0		; system call to read input to res
 	mov 	rdi, 1
 	mov 	rsi, res
 	syscall
 	
-	mov	rax, 1
+	mov	rax, 1		; system call to write res 
 	mov 	rdi, 1
 	mov 	rsi, res
 	mov 	rdx, 4
 	syscall
 		
-	mov	rax, 0x3c
+	mov	rax, 0x3c	; system call to exit 
 	xor 	rdi, rdi
 	syscall
 
 	section .data
 msg: 	db	"Enter something", 10
 
-	section .bss
-res:	resb 4
+	section .bss		; uninitialised data
+res:	resb 4			; variable to store input 
