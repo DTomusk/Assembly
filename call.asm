f84f1ddb68b9a783591f30ccd22847b21359af26
David Tomusk <43045386+DTomusk@users.noreply.github.com>
2020-06-30 11:06:22 +0100

    updated comments on call.asm

--- call.asm
+++ call.asm
@@ -2,29 +2,27 @@
 
 	section .text
 
-_start:		; try pushing onto the stack and using that value in a function 
-	mov	rbp, rsp; I don't think I have to do this right now 
+_start:		; push variable msg onto the stack and call func to print it  
+	mov	rbp, rsp	; set base pointer to access variables 
 	
-	push 	msg	; has address 402000, this is where the program goes on ret
-	call	func	; call pushes 40100d to stack
+	push 	msg	
+	call	func	
 	
-	; pop should be called here as variables were pushed on before rip
-	pop	rdi
+	pop	rdi	; pop variable that was pushed on (call pushes rip and ret pops to rip so pop has to be called after ret)
 	jmp 	finish
 	
 func:
-	;pop 	rsi 	 this actually put the return address in rsi, not msg
-	mov 	rsi, [rbp - 8]	; use base pointer to access variables 
+	mov 	rsi, [rbp - 8]	; use base pointer to access variables, variable is 64 bits (to keep stack aligned) 
 	mov 	rdi, 1
 	mov	rax, 1
-	mov	rdx, 8
+	mov	rdx, 8		; length is eight bytes 
 	syscall
 	
-	ret		; pops value into rip, value is mistakenly 402000 so tries to execute msg 
+	ret		; pops value into rip which was the value pushed by call 
 	
 finish: 
 	mov 	rax, 60
-	xor 	rdi, rdi
+	xor 	rdi, rdi	; set rdi to 0
 	syscall
 	
 	section .data
